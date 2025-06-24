ae94498fbc517437d28984bce3964427b24f69ca
aki-0517 <akki.pp.777@gmail.com>
2025-06-24 23:44:22 +0900

    add bg effect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,41 @@ import HeroSection from '../components/HeroSection';
 import HowItWorksSection from '../components/HowItWorksSection';
 import StablecoinsSection from '../components/StablecoinsSection';
 import GetYieldFromSection from "../components/GetYieldFromSection"
+import Dither from '../components/Dither';
 
 function HomePage() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-dark-blue to-black">
-      <HeroSection />
-      <HowItWorksSection />
-      <GetYieldFromSection />
-      <StablecoinsSection />
+    <div style={{ position: 'relative', minHeight: '100vh' }}>
+      {/* 背景を全画面固定（Headerの下に配置） */}
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 0,
+          pointerEvents: 'none',
+        }}
+      >
+        <Dither
+          waveColor={[0.5, 0.5, 0.5]}
+          disableAnimation={false}
+          enableMouseInteraction={true}
+          mouseRadius={0.3}
+          colorNum={4}
+          waveAmplitude={0.3}
+          waveFrequency={3}
+          waveSpeed={0.05}
+        />
+      </div>
+      {/* メインコンテンツ */}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <HeroSection />
+        <HowItWorksSection />
+        <GetYieldFromSection />
+        <StablecoinsSection />
+      </div>
     </div>
   );
 }
